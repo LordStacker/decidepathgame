862a7ef76dd23a89ee4d6a2ecc2674195a571082
Nicomatrix <nicomatryx@gmail.com>
2020-11-07 13:47:28 -0300

    Hp bar changes

--- decidepathgame.py
+++ decidepathgame.py
@@ -4,6 +4,11 @@ age = int(input("what is your age? "))
 print("Hello", name, "you are", age)
 health = 10
 print("you have starting hp of", health)
+def remove_health (damage):
+    global health
+    health -= damage
+    print("{} has recieved damage of {} and has this hp {}".format(name, damage, health))
+    
 if age >= 18:
     wants_to_play = input("Do you want to join doom?(yes/not)").lower()
     if wants_to_play == "yes":
@@ -15,14 +20,15 @@ if age >= 18:
                 print("You went around and reached the other side of the lake")
 
             elif ans == "across":
-                print("You managed to get across, but were bit by a shark and lost 5 health")
-                health -=5
+                print("You managed to get across, but were bit by a shark ")
+                remove_health(5)
             ans = input("You notice a house and a river. Which do you go to (river/house)? ").lower()
             if ans == "house":
-                print("You found a guy in the house that kick you ass out and you lose 5 health")
-                health -= 5
+                print("You found a guy in the house that kick you ass.")
+                remove_health(5)
                 if health <=0:
                     print("You have 0 HP and lost the game...")
+                    exit()
                 else:
                     print("You survived like trump")
             else:
